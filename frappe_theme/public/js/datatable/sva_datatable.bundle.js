113421a1642ce1c8daab928b9249326aa7bb9930
Samar Raj <samar.suvaidyam@gmail.com>
2025-08-20 05:33:35 +0000

    code for tooltip disabled from workflow

--- frappe_theme/public/js/datatable/sva_datatable.bundle.js
+++ frappe_theme/public/js/datatable/sva_datatable.bundle.js
@@ -426,16 +426,16 @@ class SvaDataTable {
 			const wfActionTd = document.createElement("td");
 			const el = document.createElement("select");
 			el.classList.add("form-select", "rounded");
-			const titleText = this.workflow.transitions
-				.filter(
-					(link) =>
-						frappe.user_roles.includes(link.allowed) &&
-						link.state === row[workflow_state_field]
-				)
-				.map((e) => `${e.action} by ${e.allowed}`)
-				.join("\n");
+			// const titleText = this.workflow.transitions
+			// 	.filter(
+			// 		(link) =>
+			// 			frappe.user_roles.includes(link.allowed) &&
+			// 			link.state === row[workflow_state_field]
+			// 	)
+			// 	.map((e) => `${e.action} by ${e.allowed}`)
+			// 	.join("\n");
 
-			el.setAttribute("title", titleText);
+			el.setAttribute("title", row[workflow_state_field]);
 			el.style.width = "100px";
 			el.style.minWidth = "100px";
 			el.style.padding = "2px 5px";
@@ -2290,7 +2290,8 @@ class SvaDataTable {
 						if (isClosed) {
 							el.disabled = true;
 							el.classList.add("ellipsis");
-							frappe.utils.make_popover(el, "Closed : ", row[workflow_state_field]);
+							el.setAttribute("title", row[workflow_state_field]);
+							// frappe.utils.make_popover(el, "Closed : ", row[workflow_state_field]);
 							el.innerHTML = `<option value="" style="color:black" selected disabled">${row[workflow_state_field]}</option>`;
 							el.style["-webkit-appearance"] = "none";
 							el.style["-moz-appearance"] = "none";
@@ -2313,10 +2314,11 @@ class SvaDataTable {
 								method: "frappe.model.workflow.get_transitions",
 								doc: { ...row, doctype: this.doctype },
 							});
-							const titleText = transitions
-								.map((e) => `&#x2022; ${e.action} by ${e.allowed}`)
-								.join("<br>");
-							frappe.utils.make_popover(el, `CS : ${row[workflow_state_field]}`, titleText);
+							// const titleText = transitions
+							// 	.map((e) => `&#x2022; ${e.action} by ${e.allowed}`)
+							// 	.join("<br>");
+							// frappe.utils.make_popover(el, `CS : ${row[workflow_state_field]}`, titleText);
+							el.setAttribute("title", row[workflow_state_field]);
 							el.innerHTML =
 								`<option value="" style="color:black" selected disabled class="ellipsis">${row[workflow_state_field]}</option>` +
 								[...new Set(transitions?.map((e) => e.action))]
