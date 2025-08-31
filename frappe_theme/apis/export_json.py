09d15ac8476510098df80df4ae6146c71780829d
abhirock74 <abhishek.suvaidyam@gmail.com>
2025-08-31 05:01:07 +0000

    add change in json export

--- frappe_theme/apis/export_json.py
+++ frappe_theme/apis/export_json.py
@@ -7,6 +7,7 @@ from frappe_theme.api import get_files
 
 
 def get_title(doctype, docname, as_title_field=True):
+	# print("???????????", doctype)
 	doc = frappe.db.get_value(doctype, docname, "*", as_dict=True)
 	main_doc_meta = frappe.get_meta(doctype)
 
@@ -15,7 +16,6 @@ def get_title(doctype, docname, as_title_field=True):
 		for f in main_doc_meta.fields
 		if f.fieldtype
 	]
-	# start specific field logic heare limited meta
 	if as_title_field:
 		for field in main_doc_meta.fields:
 			if field.fieldtype == "Link":
@@ -26,6 +26,10 @@ def get_title(doctype, docname, as_title_field=True):
 					doc[field.fieldname] = (
 						frappe.db.get_value(field.options, link_val, title_field) or link_val
 					)
+			elif field.fieldtype == "JSON":
+				json_data = doc.get(field.fieldname)
+				if json_data:
+					doc[field.fieldname] = json.loads(json_data)
 			elif field.fieldtype in ["Table", "Table MultiSelect"]:
 				child_doctype = field.options
 				child_meta = frappe.get_meta(child_doctype)
@@ -45,7 +49,16 @@ def get_title(doctype, docname, as_title_field=True):
 									frappe.db.get_value(child_field.options, link_val, title_field)
 									or link_val
 								)
-				child_fields_meta = [f for f in child_meta.fields if f.fieldname]
+				child_fields_meta = [
+					{
+						"fieldname": f.fieldname,
+						"fieldtype": f.fieldtype,
+						"options": f.options,
+						"label": f.label,
+					}
+					for f in child_meta.fields
+					if f.fieldname
+				]
 				if child_rows:
 					doc[field.fieldname] = {"data": child_rows, "meta": child_fields_meta}
 	for field in main_doc_meta.fields:
@@ -61,6 +74,7 @@ def get_title(doctype, docname, as_title_field=True):
 
 
 def get_related_tables(doctype, docname, exclude_meta_fields=None, as_title_field=True):
+	# print("???????????", doctype, docname)
 	main_data = get_title(doctype, docname, as_title_field)
 
 	sva_dt_config = frappe.get_doc("SVADatatable Configuration", doctype)
@@ -93,7 +107,6 @@ def get_related_tables(doctype, docname, exclude_meta_fields=None, as_title_fiel
 				table_doctype = "ToDo"
 				filters = {"reference_type": doctype, "reference_name": main_data["data"].get("name")}
 			elif child.template == "Gallery":
-				# This will return file related to doctype and docname
 				continue
 				# data = get_files(doctype, main_data["data"].get("name"))
 
@@ -107,6 +120,9 @@ def get_related_tables(doctype, docname, exclude_meta_fields=None, as_title_fiel
 				filters = {"document_type": doctype, "docname": main_data["data"].get("name")}
 
 		try:
+			if not table_doctype:
+				continue
+
 			meta = frappe.get_meta(table_doctype)
 			table_data = frappe.db.get_list(table_doctype, filters=filters, fields=["name"])
 			all_docs = []
@@ -114,7 +130,11 @@ def get_related_tables(doctype, docname, exclude_meta_fields=None, as_title_fiel
 				doc = get_title(table_doctype, row.name, as_title_field)
 				all_docs.append(doc)
 
-			fields_meta = [f for f in meta.fields if f.fieldtype not in exclude_meta_fields and f.fieldname]
+			fields_meta = [
+				{"fieldname": f.fieldname, "fieldtype": f.fieldtype, "options": f.options, "label": f.label}
+				for f in meta.fields
+				if f.fieldtype not in exclude_meta_fields and f.fieldname
+			]
 			# convert string to JSON in child table
 			if child:
 				_child = child.as_dict()
@@ -149,48 +169,15 @@ def export_json(doctype, docname, excluded_fieldtypes=None):
 		excluded_fieldtypes = ["Column Break", "Section Break", "Tab Break", "Fold", "HTML", "Button"]
 	try:
 		main_data, related_tables = get_related_tables(doctype, docname, excluded_fieldtypes, True)
-		# Structure output as per latest format
 		result = {
-			"main_table": {"data": main_data.get("data", {}), "meta": main_data.get("meta", [])},
-			"related_tables": [],
+			doctype: {"data": main_data.get("data", {}), "meta": main_data.get("meta", [])},
 		}
 		for table in related_tables:
-			result["related_tables"].append(
-				{
-					"table_doctype": table.get("table_doctype"),
-					"html_field": table.get("html_field"),
-					"data": [doc.get("data", {}) for doc in table.get("data", [])],
-					"meta": table.get("meta", []),
-					"sva_dt_meta": table.get("sva_dt_meta", {}),
-				}
-			)
-		return result
-	except Exception as e:
-		return {"error": str(e)}
-
-
-@frappe.whitelist()
-def export_json_without_meta(doctype, docname, excluded_fieldtypes=None):
-	if excluded_fieldtypes is None:
-		excluded_fieldtypes = ["Column Break", "Section Break", "Tab Break", "Fold", "HTML", "Button"]
-	try:
-		main_data, related_tables = get_related_tables(doctype, docname, excluded_fieldtypes, True)
-		# Structure output as per latest format
-		result = {
-			"main_table": {
-				"data": main_data.get("data", {}),
-				# "meta": main_data.get("meta", [])
-			},
-			"related_tables": [],
-		}
-		for table in related_tables:
-			result["related_tables"].append(
-				{
-					"table_doctype": table.get("table_doctype"),
-					"html_field": table.get("html_field"),
-					"data": [doc.get("data", {}) for doc in table.get("data", [])],
-				}
-			)
+			result[table.get("table_doctype")] = {
+				"data": [doc.get("data", {}) for doc in table.get("data", [])],
+				"meta": table.get("meta", []),
+				"sva_dt_meta": table.get("sva_dt_meta", {}),
+			}
 		return result
 	except Exception as e:
 		return {"error": str(e)}
