7722041a8f2d67f6a2eec02c791fb68a4b52c879
vishsuvaidyam <vish.suvaidyam@gmail.com>
2025-09-22 14:41:35 +0530

    changes in page wren-ai

--- frappe_theme/frappe_theme/page/wren_ai/wren_ai.js
+++ frappe_theme/frappe_theme/page/wren_ai/wren_ai.js
@@ -1,20 +1,40 @@
-frappe.pages['wren-ai'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Wren-ai',
-		single_column: true
-	});
+frappe.pages['wren-ai'].on_page_load = function (wrapper) {
+    var page = frappe.ui.make_app_page({
+        parent: wrapper,
+        title: 'Wren-ai',
+        single_column: true
+    });
 
-	let url = frappe.boot.my_theme?.embed_url ?? '';
-	if (url) {
-		$(page.parent).html(`
-			<iframe id="wrenAIFrame" src="${url}"width="100%" height="800" frameborder="0" ></iframe>
-		`);
-	}else{
-		$(page.parent).html(`
-			<div class="alert alert-danger">
-				No URL found
-			</div>
-		`);
-	}
-}
+    let url = frappe.boot.my_theme?.embed_url ?? '';
+    if (url) {
+        $(page.parent).html(`
+            <iframe id="wrenAIFrame" src="${url}" width="100%" height="800" frameborder="0"></iframe>
+        `);
+
+        let el = page.parent.querySelector('#wrenAIFrame');
+
+        function sendUser(toWindow) {
+            const dataToSend = { userId: frappe.session.user };
+            toWindow.postMessage(dataToSend, '*');
+        }
+
+        if (el) {
+            el.onload = function () {
+                sendUser(el.contentWindow);
+            };
+        }
+
+        // listen for messages from iframe
+        window.addEventListener('message', function (event) {
+            if (event && event.data && event.data.type === 'WRENAI_REQUEST_USER') {
+                sendUser(el?.contentWindow || window);
+            }
+        });
+    } else {
+        $(page.parent).html(`
+            <div class="alert alert-danger">
+                No URL found
+            </div>
+        `);
+    }
+};
