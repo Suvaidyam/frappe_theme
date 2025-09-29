3c9053151002a9ffa799c3cee93e0d9460e04fa4
abhirock74 <abhishek.suvaidyam@gmail.com>
2025-09-29 16:18:52 +0000

    add changes

--- frappe_theme/tests/controllers/sva_integrations/cloud_assets/test_s3_bucket.py
+++ frappe_theme/tests/controllers/sva_integrations/cloud_assets/test_s3_bucket.py
@@ -1,5 +1,8 @@
+import boto3
 import frappe
 from frappe.tests import IntegrationTestCase
+
+from frappe_theme.controllers.sva_integrations.cloud_assets.s3_bucket import S3Operations
 
 
 class TestS3Operations(IntegrationTestCase):
@@ -8,9 +11,29 @@ class TestS3Operations(IntegrationTestCase):
 		self.cloud_assets_doc = frappe.get_doc("Cloud Assets", "Cloud Assets").as_dict()
 
 	def test_enabled_cloud_assets(self):
+		"""
+		This test checks if the Cloud Assets integration is enabled.
+		"""
 		self.assertTrue(self.cloud_assets_doc.enable)
 
 	def test_s3_connection(self):
-		# self.s3_operations = S3Operations()
-		# self.assertIsNotNone(self.s3_operations.S3_CLIENT)
-		pass
+		"""
+		This test checks if the S3 connection can be established.
+		"""
+		if self.cloud_assets_doc.enable:
+			if self.cloud_assets_doc.provider == "AWS":
+				# client = S3Operations()
+				try:
+					pass
+					# client.S3_CLIENT.list_buckets()
+					# connection_successful = True
+				except Exception as e:
+					connection_successful = False
+					print(f"Connection failed: {e}")
+				self.assertTrue(connection_successful, "Failed to connect to AWS S3.")
+			else:
+				print("Skipping test case of AWS S3 because provider is not AWS.")
+				self.skipTest("Skipping test case of AWS S3 because provider is not AWS.")
+		else:
+			print("Cloud Assets integration is disabled")
+			self.skipTest("Cloud Assets integration is disabled.")
