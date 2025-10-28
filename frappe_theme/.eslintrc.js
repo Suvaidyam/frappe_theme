429a9529594cfeb479dbf0e6124f5a5d8005eb22
abhirock74 <abhishek.singh@dhwaniris.com>
2025-10-28 10:41:12 +0000

    changes

--- frappe_theme/.eslintrc.js
+++ frappe_theme/.eslintrc.js
@@ -1,15 +1,25 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
 	env: {
 		browser: true,
-		es2021: true,
+		node: true,
+		es2022: true,
 	},
+	parserOptions: {
+		sourceType: "module",
+	},
+	extends: ["eslint:recommended"],
 	globals: {
 		frappe: "readonly",
 		SVAHTTP: "readonly",
 		setupFieldComments: "readonly",
 	},
 	rules: {
-		"no-undef": "off",
+		// Allow known globals but still catch real undefined vars
+		"no-undef": "warn",
 		"no-unsafe-optional-chaining": "warn",
+		// Optional: stylistic consistency
+		"no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+		"no-console": "off",
 	},
 };
