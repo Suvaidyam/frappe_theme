9168bd776b2e75e2cf08aa9d404d7cfebbfbf3db
abhirock74 <abhishek.singh@dhwaniris.com>
2025-10-28 10:41:46 +0000

    changes

--- frappe_theme/.eslintrc.js
+++ frappe_theme/.eslintrc.js
@@ -1,25 +1,25 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-	env: {
-		browser: true,
-		node: true,
-		es2022: true,
-	},
-	parserOptions: {
-		sourceType: "module",
-	},
-	extends: ["eslint:recommended"],
-	globals: {
-		frappe: "readonly",
-		SVAHTTP: "readonly",
-		setupFieldComments: "readonly",
-	},
-	rules: {
-		// Allow known globals but still catch real undefined vars
-		"no-undef": "warn",
-		"no-unsafe-optional-chaining": "warn",
-		// Optional: stylistic consistency
-		"no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-		"no-console": "off",
-	},
-};
+// /** @type {import('eslint').Linter.Config} */
+// module.exports = {
+// 	env: {
+// 		browser: true,
+// 		node: true,
+// 		es2022: true,
+// 	},
+// 	parserOptions: {
+// 		sourceType: "module",
+// 	},
+// 	extends: ["eslint:recommended"],
+// 	globals: {
+// 		frappe: "readonly",
+// 		SVAHTTP: "readonly",
+// 		setupFieldComments: "readonly",
+// 	},
+// 	rules: {
+// 		// Allow known globals but still catch real undefined vars
+// 		"no-undef": "warn",
+// 		"no-unsafe-optional-chaining": "warn",
+// 		// Optional: stylistic consistency
+// 		"no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+// 		"no-console": "off",
+// 	},
+// };
