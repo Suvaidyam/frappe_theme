ee844de53c76ae84449d211910b087c3c9e3c29a
Samar Singh <samar.suvaidyam@gmail.com>
2025-11-01 03:53:18 +0000

    feat: HTML from API logic handling

--- frappe_theme/public/js/overwrite_form.bundle.js
+++ frappe_theme/public/js/overwrite_form.bundle.js
@@ -29,6 +29,7 @@ import SVAmGrantTask from "./custom_components/task.bundle.js";
 import SVATimelineGenerator from "./custom_components/timeline.bundle.js";
 import CustomApprovalRequest from "./custom_components/approval_request/approval_request.bundle.js";
 import SVAExcel from "./sva_excel/excel.bundle.js";
+import CustomDynamicHtml from "./custom_components/dynamic_html/dynamic_html.bundle.js";
 
 frappe.ui.form.Form = class CustomForm extends frappe.ui.form.Form {
 	constructor(...args) {
@@ -633,6 +634,7 @@ frappe.ui.form.Form = class CustomForm extends frappe.ui.form.Form {
 			"Linked Users": SVALinkedUser,
 			"Approval Request": CustomApprovalRequest,
 			Excel: SVAExcel,
+			"HTML View From API" : CustomDynamicHtml
 		};
 		return componentMap[template];
 	}
