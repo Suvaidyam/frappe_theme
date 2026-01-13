e2785cc3c3a64957b8c4d463f37816bd1c9613a8
Samar Singh <samar.suvaidyam@gmail.com>
2026-01-13 05:53:04 +0000

    changes for events binding

--- frappe_theme/frappe_theme/doctype/approval_tracker/approval_tracker.js
+++ frappe_theme/frappe_theme/doctype/approval_tracker/approval_tracker.js
@@ -103,11 +103,11 @@ const show_table = async (frm, document_type) => {
 	wf_field = wf_field?.message?.workflow_state_field;
 
 	await frappe.require("sva_datatable.bundle.js");
-
 	frm["sva_dt_instance"] = new frappe.ui.SvaDataTable({
 		wrapper: wrapper,
 		frm: Object.assign(frm, {
 			dt_events: {
+				...(frm.dt_events || {}),
 				[document_type]: {
 					...(frm.dt_events?.[document_type] || {}),
 					before_load: async function (dt) {
