19586b657025ddad26aca97b34416c84058d5c89
Samar Singh <samar.suvaidyam@gmail.com>
2026-01-22 06:03:19 +0000

    changes

--- frappe_theme/public/js/override_table_multiselect.bundle.js
+++ frappe_theme/public/js/override_table_multiselect.bundle.js
@@ -1,24 +1,24 @@
-frappe.ui.form.ControlTableMultiSelect = class SVAControlTableMultiSelect extends (
-	frappe.ui.form.ControlTableMultiSelect
-) {
-	get_link_field() {
-		if (!this._link_field) {
-			const meta = frappe.get_meta(this.df.options);
-			this._link_field =
-				meta?.fields?.find((df) => df.fieldtype === "Link") ||
-				this.df?.fields?.find((df) => df.fieldtype === "Link");
-			if (!this._link_field) {
-				throw new Error("Table MultiSelect requires a Table with atleast one Link field");
-			}
-		}
-		return this._link_field;
-	}
-	set_pill_html(values) {
-		const html = values.map((value) => this.get_pill_html(value)).join("");
-		$(this.input_area).find(".tb-selected-value").remove();
-		$(this.input_area).prepend(html);
-	}
-};
+// frappe.ui.form.ControlTableMultiSelect = class SVAControlTableMultiSelect extends (
+// 	frappe.ui.form.ControlTableMultiSelect
+// ) {
+// 	get_link_field() {
+// 		if (!this._link_field) {
+// 			const meta = frappe.get_meta(this.df.options);
+// 			this._link_field =
+// 				meta?.fields?.find((df) => df.fieldtype === "Link") ||
+// 				this.df?.fields?.find((df) => df.fieldtype === "Link");
+// 			if (!this._link_field) {
+// 				throw new Error("Table MultiSelect requires a Table with atleast one Link field");
+// 			}
+// 		}
+// 		return this._link_field;
+// 	}
+// 	set_pill_html(values) {
+// 		const html = values.map((value) => this.get_pill_html(value)).join("");
+// 		$(this.input_area).find(".tb-selected-value").remove();
+// 		$(this.input_area).prepend(html);
+// 	}
+// };
 
 frappe.form.formatters["TableMultiSelect"] = function (rows, df, options) {
 	rows = rows || [];
