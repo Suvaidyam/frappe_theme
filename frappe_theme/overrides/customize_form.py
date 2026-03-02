cac6bf0253433d541ab2602a0f28eb181e0d43ec
Samar Singh <samar.suvaidyam@gmail.com>
2026-03-02 06:41:21 +0000

    code for allowing customization of single doctypes if it is dashboard

---
+++ frappe_theme/overrides/customize_form.py
@@ -0,0 +1,17 @@
+import frappe
+from frappe import _
+from frappe.custom.doctype.customize_form.customize_form import CustomizeForm
+from frappe.model import core_doctypes_list
+
+class SVACustomizeForm(CustomizeForm):
+    def validate_doctype(self, meta):
+        """
+        Check if the doctype is allowed to be customized.
+        """
+        if self.doc_type in core_doctypes_list:
+            frappe.throw(_("Core DocTypes cannot be customized."))
+        if meta.issingle and (meta.get("is_dashboard", 0) or 0) == 0:
+            frappe.throw(_("Single DocTypes cannot be customized."))
+
+        if meta.custom:
+            frappe.throw(_("Only standard DocTypes are allowed to be customized from Customize Form."))
