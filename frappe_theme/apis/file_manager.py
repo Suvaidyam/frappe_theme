80c11a93f3ff3bf1a900302f61d5cc84afdaee98
Samar Singh <samar.suvaidyam@gmail.com>
2026-03-06 08:10:09 +0000

    removed commented code

--- frappe_theme/apis/file_manager.py
+++ frappe_theme/apis/file_manager.py
@@ -1,32 +1,16 @@
-# import frappe
-# import os
+import os
+from io import BytesIO
 
-# @frappe.whitelist()
-# def download_file(file_path):
-#     base_url = frappe.utils.get_url()
-#     file_path = f"{base_url}/{file_path.lstrip('/')}"
-#     # file_path = frappe.get_site_path(file_path)
-    
-#     print(file_path,'=========================file_path===========================')
-#     if not os.path.exists(file_path):
-#         frappe.throw("File not found")
-#     with open(file_path, "rb") as f:
-#         file_content = f.read()
-        
-#     frappe.local.response.filename = os.path.basename(file_path)
-#     frappe.local.response.filecontent = file_content
-#     frappe.local.response.type = "download"
-    
 import frappe
 from frappe.utils.file_manager import get_file
-from io import BytesIO
-import os
+
+
 @frappe.whitelist()
 def download_file(file_path):
-    file_data = get_file(file_path)
-    file_name = os.path.basename(file_data[0])
-    file_content = file_data[1]
-    file_stream = BytesIO(file_content)
-    frappe.local.response.filename = file_name
-    frappe.local.response.filecontent = file_stream.getvalue()
-    frappe.local.response.type = "download"
+	file_data = get_file(file_path)
+	file_name = os.path.basename(file_data[0])
+	file_content = file_data[1]
+	file_stream = BytesIO(file_content)
+	frappe.local.response.filename = file_name
+	frappe.local.response.filecontent = file_stream.getvalue()
+	frappe.local.response.type = "download"
